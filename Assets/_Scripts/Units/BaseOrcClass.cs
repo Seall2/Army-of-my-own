f770960a0d4df8b43b00ebfa9e16a758ed444393
vidar <unknown-user@fe80::416a:932a:d4db:f847%8>
2015-05-18 13:21:28 +0200

    OK

---
+++ Assets/_Scripts/Units/BaseOrcClass.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseOrcClass : BaseUnitClass
+{
+	public BaseOrcClass()
+	{
+		BaseUnitName = "Orc";
+		BaseUnitDescription = "An orc with great fury";
+		BaseUnitImg = null;
+
+		Health = 120;
+		Attack = 10;
+		Defence = 19;
+		Speed = 33;
+
+		FireResistence = 0.0f;
+		WaterResistence = 0.0f;
+		EarthResistence = 0.3f;
+		AirResistence = 0.0f;
+		ShadowResistence = 0.0f;
+		LightResistence = 0.0f;
+
+		BasicAttack = "SMACK";
+		Support = "ROAR(attack boost)";
+
+		Ability = "CHARGE";
+
+		Upgrade = "ORC 2";
+		Mutate = "ORC MUTANT";
+		Rebuild = "ORC CYBORG";
+	}
+}
---
+++ Assets/_Scripts/Units/BaseUnit.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseUnit
+{
+	private string unitName;
+	private BaseUnitClass unitClass;
+
+	private int health;
+	private int attack;
+	private int defence;
+	private int speed;
+
+	private float fireResistence;
+	private float waterResistence;
+	private float earthResistence;
+	private float airResistence;
+	private float shadowResistence;
+	private float lightResistence;
+
+	public string UnitName
+	{
+		get{ return unitName;}
+		set{ unitName = value;}
+	}
+
+	public int Health
+	{
+		get{ return health;}
+		set{ health = value;}
+	}
+	
+	public int Attack
+	{
+		get{ return attack;}
+		set{ attack = value;}
+	}
+	
+	public int Defence
+	{
+		get{ return defence;}
+		set{ defence = value;}
+	}
+	
+	public int Speed
+	{
+		get{return speed;}
+		set{ speed = value;}
+	}
+	
+	public float FireResistence
+	{
+		get{ return fireResistence;}
+		set{ fireResistence = value;}
+	}
+	
+	public float WaterResistence
+	{
+		get{ return waterResistence;}
+		set{ waterResistence = value;}
+	}
+	
+	public float EarthResistence
+	{
+		get{ return earthResistence;}
+		set{ earthResistence = value;}
+	}
+	
+	public float AirResistence
+	{
+		get{ return airResistence;}
+		set{ airResistence = value;}
+	}
+	
+	public float ShadowResistence
+	{
+		get{ return shadowResistence;}
+		set{ shadowResistence = value;}
+	}
+	
+	public float LightResistence
+	{
+		get{ return lightResistence;}
+		set{ lightResistence = value;}
+	}
+}
