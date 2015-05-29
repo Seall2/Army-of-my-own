1d7b41db57129b8bda415da220e6d97350693c93
Benjamin <unknown-user@fe80::dd41:596f:b1d3:2f31%22>
2015-05-29 11:29:18 +0200

    again again

---
+++ Assets/_Scripts/Buildings/BaseBuildingClass.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseBuildingClass {
+
+	private int id;
+	private int level;
+	private string buildingName;
+	private string buildingDescription;
+
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int Id{ get; set;}
+
+	public int Level{ get; set;}
+
+	public string BuildingName{ get; set;}
+
+	public string BuildingDescription{ get; set;}
+}
---
+++ Assets/_Scripts/Buildings/BaseGoblinoidBuildingClass.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseGoblinoidBuildingClass : BaseBuildingClass {
+
+	public BaseGoblinoidBuildingClass()
+	{
+		Id = 1;
+		BuildingName = "Goblinoid Barrack";
+		Level = 1;
+		BuildingDescription = "The Goblinoid Barrack Is A Place Where You Can Recruit And Upgrade Goblins, Orcs And Trolls";
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
